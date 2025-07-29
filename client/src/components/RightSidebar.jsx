500f2b1fe74f98e660f6e6cd2f0ad68aad8c648a
adarshhh25 <adarshjha4425@gmail.com>
2025-07-29 22:08:18 +0530

    Frontend Done

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -21,7 +21,7 @@ const RightSidebar = ({selectedUser}) => {
           <p>Media</p>
           <div className='mt-2 max-h-[200px] overflow-y-scroll grid grid-cols-2 gap-4 opacity-80'>
               {imagesDummyData.map((url, index) => (
-                <div key={index} onClick={window.open(url)}
+                <div key={index} onClick={() =>window.open(url)}
                 className='cursor-pointer rounded'>
                    <img src={url} alt="" className='h-full rounded-md' />
                 </div>
