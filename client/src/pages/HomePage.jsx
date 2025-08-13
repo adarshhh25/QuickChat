f79d02e9be911b4f60879bba1d8586b3ed92af39
adarshhh25 <adarshjha4425@gmail.com>
2025-08-13 22:08:15 +0530

    Changes

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
           overflow-hidden h-[100%] grid grid-cols-1 relative ${selectedUser ? 
           'md:grid-cols-[1fr_1.5fr_1fr] xl:grid-cols-[1fr_2fr_1fr]' : 'md:grid-cols-2'}`}>
           <Sidebar/>
-          <ChatContainer selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
+          <ChatContainer/>
           <RightSidebar selectedUser={selectedUser} setSelectedUser={setSelectedUser}/>
         </div>
     </div>
